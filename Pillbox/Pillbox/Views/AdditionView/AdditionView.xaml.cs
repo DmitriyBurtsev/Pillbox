868063beb253713adf71d643e5d7b74deaf3f454
Дмитрий Бурцев <dmitrybyrcev93@gmail.com>
2021-06-30 16:56:08 +0300

    AddPageDone

--- Pillbox/Pillbox/Views/AdditionView/AdditionView.xaml.cs
+++ Pillbox/Pillbox/Views/AdditionView/AdditionView.xaml.cs
@@ -43,7 +43,7 @@ namespace Pillbox.Views.AdditionView
             for (double i = 1; i < 100; i+=0.5)
             {
                 dosagePicker.Items.Add(Convert.ToString(i));
-            }
+            }           
             
             //Binding daysDurationBinding = new Binding { Source = daysStepper, Path = "Value" };
             //daysLabel.SetBinding(Label.TextProperty, daysDurationBinding);
