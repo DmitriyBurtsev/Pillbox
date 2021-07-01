f26813785c5007e003f9de9257bebb2549a6809f
Дмитрий Бурцев <dmitrybyrcev93@gmail.com>
2021-07-01 23:50:46 +0300

    test1

--- Pillbox/Pillbox/App.xaml.cs
+++ Pillbox/Pillbox/App.xaml.cs
@@ -10,31 +10,15 @@ namespace Pillbox
 {
     public partial class App : Application
     {
-        public const string DbFileName = "medicines.db";
-
-        public static string dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DbFileName);
-
-        public static MedicineDatabase database;
-        public static MedicineDatabase Database
-        {
-            get
-            {
-                if (database == null)
-                {
-                    database = new MedicineDatabase(dbpath);
-                }
-                return database;
-            }
-        }
+        
         public App()
         {
             InitializeComponent();            
             MainPage = new AppShell();
         }
 
-        protected override async void OnStart()
+        protected override void OnStart()
         {
-            await Database.CreateTable();
         }
 
         protected override void OnSleep()
